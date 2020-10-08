cc5bfbe9414fd2af579c81b9581302ce87162312
anthivaios <44756424+anthivaios@users.noreply.github.com>
2020-10-08 18:01:42 +0300

    Add files via upload

---
+++ circularEconomy/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, url_for, render_template, request
+from allTogether import allTogether
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/form", methods=["POST", "GET"])
+def form():
+    if request.method == "POST":
+
+        data = allTogether(request)
+        data.allInitiate(request)
+
+        return render_template("results.html", value=data)
+    else:
+        return render_template("form.html")
+
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
